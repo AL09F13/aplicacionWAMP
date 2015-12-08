7c77a6baeb5ee6a3686b5091bfcaf14eb6ba709e
AL09F13 <c4rt3r97@gmail.com>
2015-12-08 09:13:06 -0600

    Dos
    Igual y sirve

--- js/acciones.js
+++ js/acciones.js
@@ -6,13 +6,16 @@ document.addEventListener("deviceready",function(){
 			type: "POST",
 			url: "http://192.168.1.177/Practica03/buscarjuegos.php",
 			error: function(){
-				alert ("no me puedo conectar al  servidor de jegos");
-			},
-			success: function(respuesta){
-				   '<div id="imagen" style="display: inline-block"> <img src="" width="175" height="111"> </div> <div id="datos" style="display: inline-block"> <div> <p>Titulo</p></div> <div><p>Fecha</p></div> <div><p>Compañia</p></div> <div><p>Plataforma</p></div> <div><p>Genero</p></div> <div><p>Clasificacion</p></div></div>'
+			alert ("no me puedo conectar al  servidor de jegos");},
+			
+			success: function(respuesta){ var producto = JSON.parse(respuesta);
+			$('#contenido').empty();
+			for(var $x=0; $x < producto.peliculas.length;$x++)
+			{$('#contenido').append('<div id="imagen" style="display: inline-block"> <img src="" width="175" height="111"> </div> <div id="datos" style="display: inline-block"> <div> <h2>Titulo:</h2> <p>'+ producto.juegos[$x].titulo +'</p></div> <div><h2>Fecha:</h2> <p>'+ producto.juegos[$x].fecha +'</p></div> <div><h2>Compañia:</h2> <p>'+ producto.juegos[$x].compania +'</p></div> <div><h2>Plataformas:</h2><p>'+ producto.juegos[$x].plataformas +'</p></div><div><h2>Genero:</h2<p>'+ producto.juegos[$x].genero +'</p>></div> <div><h2>Clasificación:</h2><p>'+ producto.juegos[$x].clasificacion +'</p></div></div>')
+				   
 			}
 			
-			
+			}
 			
 		});
 	});
